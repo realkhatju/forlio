83590d46590722762785b8b8c2c1c6b5d1aaa26c
realkhatju <programmercruso15@gmail.com>
2024-09-16 20:31:19 +0630

    Second Commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home(){
   return (
     <div>
       <h1>Built by Kaung Htat</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis laboriosam fugiat ducimus explicabo deleniti quos tempora sapiente repudiandae aperiam, quasi consectetur. Dolorum et recusandae nostrum iste exercitationem eveniet eaque in.</p>
     </div>
   )
 }
